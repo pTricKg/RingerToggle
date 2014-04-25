c7443125ad7ff04167d90bb46d3afde1bd1b7b47
pTricKg <ptrickg1976@gmail.com>
2014-04-24 22:26:23 -0600

    debuggin(not sure), xml cleaning, code cleaning, etc darkened vibrate icon before but forgot to mention.. played around with layouts

--- PhoneVolumeToggle/src/com/pTricKg/PhoneVolumeToggle/AppWidget.java
+++ PhoneVolumeToggle/src/com/pTricKg/PhoneVolumeToggle/AppWidget.java
@@ -12,58 +12,66 @@ import android.media.AudioManager;
 import android.widget.RemoteViews;
 
 public class AppWidget extends AppWidgetProvider {
-@Override
+	@Override
 	public void onReceive(Context context, Intent intent) {
 		if (intent.getAction() == null) {
-			context.startService(new Intent(context, ToggleService.class)); 
-		}else {
+			context.startService(new Intent(context, ToggleService.class));
+		} else {
 			super.onReceive(context, intent);
 		}
-}
+	}
+
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-	int[] appWidgetIds) {
-		
+			int[] appWidgetIds) {
+
 		context.startService(new Intent(context, ToggleService.class));
 	}
-	
+
 	public static class ToggleService extends IntentService {
 		public ToggleService() {
 			super("AppWidget$ToggleService");
 		}
-		
+
 		@Override
 		protected void onHandleIntent(Intent intent) {
 			ComponentName me = new ComponentName(this, AppWidget.class);
 			AppWidgetManager mgr = AppWidgetManager.getInstance(this);
 			mgr.updateAppWidget(me, buildUpdate(this));
 		}
-		
+
 		private RemoteViews buildUpdate(Context context) {
-			RemoteViews updateViews = new
-			RemoteViews(context.getPackageName(),R.layout.widget);
-			
-			AudioManager audioManager = (AudioManager)context.getSystemService(Activity.AUDIO_SERVICE);
-				
-				if(audioManager.getRingerMode() == AudioManager.RINGER_MODE_SILENT) {
-					updateViews.setImageViewResource(R.id.phoneState, R.drawable.phone_on);
-					
-					audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-				}else if(audioManager.getRingerMode() == AudioManager.RINGER_MODE_NORMAL) {
-					updateViews.setImageViewResource(R.id.phoneState, R.drawable.phone_vibrate);
-					
-					audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
-				}else {
-					updateViews.setImageViewResource(R.id.phoneState, R.drawable.phone_silent);
-					
-					audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-				}
-				Intent i = new Intent(this, AppWidget.class);
-				
-				PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
-				updateViews.setOnClickPendingIntent(R.id.phoneState, pi);
-				return updateViews;
+
+			RemoteViews updateViews = new RemoteViews(context.getPackageName(),
+					R.layout.widget);
+
+			AudioManager audioManager = (AudioManager) context
+					.getSystemService(Activity.AUDIO_SERVICE);
+
+			if (audioManager.getRingerMode() == AudioManager.RINGER_MODE_SILENT) {
+				updateViews.setImageViewResource(R.id.phoneState,
+						R.drawable.phone_on);
+
+				audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+			} else if (audioManager.getRingerMode() == AudioManager.RINGER_MODE_NORMAL) {
+				updateViews.setImageViewResource(R.id.phoneState,
+						R.drawable.phone_vibrate);
+
+				audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+			} else {
+				updateViews.setImageViewResource(R.id.phoneState,
+						R.drawable.phone_silent);
+
+				audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+			}
+
+			Intent i = new Intent(this, AppWidget.class);
+
+			// intent to get other activity processes and update widget view
+			PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
+			updateViews.setOnClickPendingIntent(R.id.phoneState, pi);
+			return updateViews;
 		}
-		
+
 	}
 }
--- PhoneVolumeToggle/src/com/pTricKg/PhoneVolumeToggle/PhoneVolumeToggleActivity.java
+++ PhoneVolumeToggle/src/com/pTricKg/PhoneVolumeToggle/PhoneVolumeToggleActivity.java
@@ -7,127 +7,133 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-
 public class PhoneVolumeToggleActivity extends Activity {
-    	
+
 	private AudioManager mAudioManager;
 	private boolean mPhoneIsSilent;
 	private boolean mPhoneIsVibrate;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        //initializing AudioManager variable
-        mAudioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
-        
-        //initializing Ringer State check
-        checkIfPhoneIsSilent();
-        
-        //initialize button click listener. 
-        //place here to allow ringer state check first, I think!
-        setButtonClickListener();
-    }
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// initializing AudioManager variable
+		mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+		// initializing Ringer State check
+		checkIfPhoneIsSilent();
+
+		// initialize button click listener.
+		// place here to allow ringer state check first, I think!
+		setButtonClickListener();
+	}
 
 	private void setButtonClickListener() {
-		
-		ImageButton toggleButton = (ImageButton)findViewById(R.id.toggleButton);
-        toggleButton.setOnClickListener(new View.OnClickListener() {
-			
+		/*
+		 * maybe issue with invoking without asking for it.. has something to do
+		 * with using ImageButton instead of Button.. now trying fix with
+		 * removal of auto-updater..
+		 */
+		ImageButton toggleButton = (ImageButton) findViewById(R.id.toggleButton);
+		toggleButton.setOnClickListener(new View.OnClickListener() {
+
 			public void onClick(View v) {
-  
-				//this checks to see what to set
-				//if silent, turns on, if on , turn off. otherwise, switches to vibrate mode
-				if(mPhoneIsSilent) {
-					//change to Normal
+				// this checks to see what to set
+				// if silent, turns on, if on , turn off. otherwise, switches to
+				// vibrate mode
+				if (mPhoneIsSilent) {
+					// change to Normal
 					mAudioManager
-						.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+							.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
 					mPhoneIsSilent = false;
 					mPhoneIsVibrate = false;
-					
-//					Context context = getApplicationContext();
-//					CharSequence text = "Ringer On";
-//					int duration = Toast.LENGTH_SHORT;
-//
-//					Toast toast = Toast.makeText(context, text, duration);
-//					toast.show();
-					
-				}else if(mPhoneIsVibrate){
-					//change to silent
+
+					// Context context = getApplicationContext();
+					// CharSequence text = "Ringer On";
+					// int duration = Toast.LENGTH_SHORT;
+					//
+					// Toast toast = Toast.makeText(context, text, duration);
+					// toast.show();
+
+				} else if (mPhoneIsVibrate) {
+					// change to silent
 					mAudioManager
-						.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+							.setRingerMode(AudioManager.RINGER_MODE_SILENT);
 					mPhoneIsSilent = true;
 					mPhoneIsVibrate = false;
-					
-//					Context context = getApplicationContext();
-//					CharSequence text = "Ringer Silenced";
-//					int duration = Toast.LENGTH_SHORT;
-//
-//					Toast toast = Toast.makeText(context, text, duration);
-//					toast.show();
-					
-				}else{
+
+					// Context context = getApplicationContext();
+					// CharSequence text = "Ringer Silenced";
+					// int duration = Toast.LENGTH_SHORT;
+					//
+					// Toast toast = Toast.makeText(context, text, duration);
+					// toast.show();
+
+				} else {
 					mAudioManager
-					.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+							.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
 					mPhoneIsVibrate = true;
 					mPhoneIsSilent = false;
-					
-//					Context context = getApplicationContext();
-//					CharSequence text = "Ringer vibrate";
-//					int duration = Toast.LENGTH_SHORT;
-//
-//					Toast toast = Toast.makeText(context, text, duration);
-//					toast.show();
+
+					// Context context = getApplicationContext();
+					// CharSequence text = "Ringer vibrate";
+					// int duration = Toast.LENGTH_SHORT;
+					//
+					// Toast toast = Toast.makeText(context, text, duration);
+					// toast.show();
 				}
 
-				//calling to toggle UI
+				// calling to toggle UI
 				toggleUi();
-				
+
 			}
 
-			});
-        
+		});
+
 	}
+
 	private void checkIfPhoneIsSilent() {
-		
+
 		int ringerMode = mAudioManager.getRingerMode();
 		if (ringerMode == AudioManager.RINGER_MODE_SILENT) {
 			mPhoneIsSilent = true;
 			mPhoneIsVibrate = false;
-		}else if (ringerMode == AudioManager.RINGER_MODE_NORMAL){
+		} else if (ringerMode == AudioManager.RINGER_MODE_NORMAL) {
 			mPhoneIsSilent = false;
 			mPhoneIsVibrate = false;
-		}else if(ringerMode == AudioManager.RINGER_MODE_VIBRATE){
+		} else if (ringerMode == AudioManager.RINGER_MODE_VIBRATE) {
 			mPhoneIsSilent = false;
 			mPhoneIsVibrate = true;
-		}else{
+		} else {
 			mPhoneIsSilent = false;
 			mPhoneIsVibrate = false;
 		}
 	}
 
-//makes layout switch
-private void toggleUi() {
-		
-	ImageButton imageButton = (ImageButton) findViewById(R.id.toggleButton);
-	Drawable newPhoneImage;
-	
-	if (mPhoneIsSilent) {
-		newPhoneImage = getResources().getDrawable(R.drawable.phone_silent);
-	}else if (mPhoneIsVibrate){
-		newPhoneImage = getResources().getDrawable(R.drawable.phone_vibrate);
-	}else{
-		newPhoneImage = getResources().getDrawable(R.drawable.phone_on);
+	// makes layout switch
+	private void toggleUi() {
+
+		ImageButton imageButton = (ImageButton) findViewById(R.id.toggleButton);
+		Drawable newPhoneImage;
+
+		if (mPhoneIsSilent) {
+			newPhoneImage = getResources().getDrawable(R.drawable.phone_silent);
+		} else if (mPhoneIsVibrate) {
+			newPhoneImage = getResources()
+					.getDrawable(R.drawable.phone_vibrate);
+		} else {
+			newPhoneImage = getResources().getDrawable(R.drawable.phone_on);
+		}
+		imageButton.setImageDrawable(newPhoneImage);
+
 	}
-	imageButton.setImageDrawable(newPhoneImage);
-	
-}
-//makes sure to check ringer state when user resumes activity
-@Override
-protected void onResume() {
-	super.onResume();
-	checkIfPhoneIsSilent();
-	toggleUi();
-}
+
+	// makes sure to check ringer state when user resumes activity
+	@Override
+	protected void onResume() {
+		super.onResume();
+		checkIfPhoneIsSilent();
+		toggleUi();
+	}
 }
