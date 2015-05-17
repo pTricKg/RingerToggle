e7df2e0dae3ed403f9f24a27204406f521f5af6c
pTricKg <ptrickg1976@gmail.com>
2015-05-17 14:48:03 -0600

    removed unneeded trans files

--- PhoneVolumeToggle/gen/com/pTricKg/PhoneVolumeToggle/R.java
+++ PhoneVolumeToggle/gen/com/pTricKg/PhoneVolumeToggle/R.java
@@ -29,10 +29,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050001;
-        public static final int counter=0x7f050003;
         public static final int hello=0x7f050000;
-        public static final int nTime=0x7f050002;
-        public static final int toggle_silent_mode=0x7f050004;
     }
     public static final class xml {
         public static final int global_tracker=0x7f040000;
