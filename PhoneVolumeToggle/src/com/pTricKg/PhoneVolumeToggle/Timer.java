cef6a458ae01e0ac1b994b244d1965e8900062ab
pTricKg <ptrickg1976@gmail.com>
2016-05-21 21:21:05 -0600

    interface is setup

--- PhoneVolumeToggle/src/com/pTricKg/PhoneVolumeToggle/Timer.java
+++ PhoneVolumeToggle/src/com/pTricKg/PhoneVolumeToggle/Timer.java
@@ -1,19 +1,56 @@
 package com.pTricKg.PhoneVolumeToggle;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.TimePickerDialog;
+import android.widget.TimePicker;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import java.util.Calendar;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by pTricKg on 5/21/2016.
  */
-public class Timer extends Activity {
+public class Timer extends Activity{
+
+    public static TimePicker timePicker;
+    final Calendar c = Calendar.getInstance();
+    int hour = c.get(Calendar.HOUR_OF_DAY);
+    int min = c.get(Calendar.MINUTE);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timer);
+
+        //timepicker setup
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(false);
+        //another button for timer
+        final Button timerButton = (Button) findViewById(R.id.button);
+        timerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                makeToast();
+
+                }
+        });
     }
-    //Intent intent = getIntent();
+
+
+    private void makeToast() {
+
+        Toast.makeText(this, "Blah,Blah,Blah " + hour + ":" + min, Toast.LENGTH_LONG).show();
+    }
+
+
 }
+
+
