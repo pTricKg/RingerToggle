7e114ce6a8956046eaa4aa9ba4d3a723e8849ee4
ptrickg <ptrickg1976@gmail.com>
2016-05-28 22:24:40 -0600

    added note and clean up files

--- PhoneVolumeToggle/src/com/pTricKg/PhoneVolumeToggle/PhoneVolumeToggleActivity.java
+++ PhoneVolumeToggle/src/com/pTricKg/PhoneVolumeToggle/PhoneVolumeToggleActivity.java
@@ -1,5 +1,22 @@
 package com.pTricKg.PhoneVolumeToggle;
 
+/*RingerToggle
+Copyright (C) 2014 Patrick Gorman
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.*/
+
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
@@ -8,6 +25,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +34,7 @@ import android.widget.ImageButton;
 /* Still WIP */
 
 public class PhoneVolumeToggleActivity extends Activity {
-
+	
 	private static final String TAG = "PhoneVolumeToggleActivity";
 
 	private AudioManager mAudioManager;
@@ -38,6 +56,9 @@ public class PhoneVolumeToggleActivity extends Activity {
 		// initialize button click listener.
 		// place here to allow ringer state check first, I think!
 		setButtonClickListener();
+<<<<<<< HEAD
+		
+=======
 
 		//another button for timer
 		Button startButton = (Button) findViewById(R.id.timerButton);
@@ -48,6 +69,7 @@ public class PhoneVolumeToggleActivity extends Activity {
 			}
 		});
 
+>>>>>>> a4b592df03c5ac0edbb0b18316c4c8c50dfebd0d
 	}
 	private void openTimer(){
 
